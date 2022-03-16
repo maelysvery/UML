3c5beb8c3eba7e37cae0e1f82586e0c7f2679fee
maelysvery <73127453+maelysvery@users.noreply.github.com>
2022-03-16 16:23:44 +0100

    Update RequestMenu.java

--- Project_UML/src/com/company/RequestMenu.java
+++ Project_UML/src/com/company/RequestMenu.java
@@ -313,8 +313,13 @@ public class RequestMenu {
         System.out.println("Option A : Let them land");
         System.out.println("Option B : Refuse them\n");
 
-        System.out.print("Enter your choice (A or B) : ");
-        String choice = scanner.nextLine();
+        boolean test = false;
+        
+        while(!test) 
+        {
+            System.out.print("Enter your choice (A or B) : ");
+            String choice = scanner.nextLine();
+        }
 
         System.out.println("\n");
 
@@ -331,8 +336,13 @@ public class RequestMenu {
         System.out.println("Option B : Get police involved");
         System.out.println("Option C : Let the planes land anyways\n");
 
-        System.out.print("Enter your choice (A, B or C) : ");
-        String choice = scanner.nextLine();
+        boolean test = false;
+        
+        while(!test) 
+        {
+            System.out.print("Enter your choice (A or B) : ");
+            String choice = scanner.nextLine();
+        }
 
         System.out.println("\n");
 
@@ -348,8 +358,13 @@ public class RequestMenu {
         System.out.println("Option A : Lockdown the runway");
         System.out.println("Option B : Let the man go\n");
 
-        System.out.print("Enter your choice (A or B) : ");
-        String choice = scanner.nextLine();
+        boolean test = false;
+        
+        while(!test) 
+        {
+            System.out.print("Enter your choice (A or B) : ");
+            String choice = scanner.nextLine();
+        }
 
         System.out.println("\n");
 
